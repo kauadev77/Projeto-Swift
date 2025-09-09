c2c5044c1a61892c50c875907a6eef1c3f1adabc
Kauã de Jesus Soares <kauaje18@gmail.com>
2025-09-09 13:20:18 -0300

    Update main.swift Parte 1 finalizada

--- Sources/Projeto/main.swift
+++ Sources/Projeto/main.swift
@@ -1,6 +1,10 @@
-import Foundation
+import Foundation 
 
-class Pessoa{
+// ---------------------------
+// Parte 1 - Pessoas
+// ---------------------------
+
+class Pessoa {
     var nome: String
     var email: String
 
@@ -14,42 +18,74 @@ class Pessoa{
     }
 }
 
-enum NivelAluno{
+enum NivelAluno {
     case iniciante
     case intermediario
     case avancado
 }
 
-class Aluno:Pessoa{
+class Aluno: Pessoa {
     var matricula: String
-    var nivel: NivelAluno
-    var plano: Plano
+    var nivel: NivelAluno = .iniciante   // valor padrão
+    private(set) var plano: Plano        // restrito para alteração
 
     init(nome: String, email: String, matricula: String, plano: Plano) {
+        self.matricula = matricula
+        self.plano = plano
         super.init(nome: nome, email: email)
-
     }
-    
+
+    override func getDescricao() -> String {
+        return super.getDescricao() +
+               ", Matrícula: \(matricula), Nível: \(nivel), Plano: \(plano.nome) - Mensalidade: R$ \(plano.calcularMensalidade())"
+    }
 }
 
+class Instrutor: Pessoa {
+    var especialidade: String
 
-// Parte 2 (Augusto)
+    init(nome: String, email: String, especialidade: String) {
+        self.especialidade = especialidade
+        super.init(nome: nome, email: email)
+    }
 
-class Plano{
+    override func getDescricao() -> String {
+        return super.getDescricao() + ", Especialidade: \(especialidade)"
+    }
+}
+
+// ---------------------------
+// Parte 2 - Planos
+// ---------------------------
+
+class Plano {
     private(set) var nome: String
-    init (nome: String) {
+    
+    init(nome: String) {
         self.nome = nome
     }
-    func mensalidade() -> Double {
+
+    func calcularMensalidade() -> Double {
         return 0.0
     }
 }    
-class PlanoMensal: Plano{
 
-    init(nome: String) {
-        super.init(nome: "PlanoMensal")
+class PlanoMensal: Plano {
+    override init(nome: String = "Plano Mensal") {
+        super.init(nome: nome)
     }
-    override func calcularMensalidade() {
+
+    override func calcularMensalidade() -> Double {
         return 120.0
+    }
+}
+
+class PlanoAnual: Plano {
+    override init(nome: String = "Plano Anual (Promocional)") {
+        super.init(nome: nome)
+    }
+
+    override func calcularMensalidade() -> Double {
+        return (1440 * 0.8) / 12
     }
 }
